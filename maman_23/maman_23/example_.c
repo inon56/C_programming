51f889fe2ca9a5ba0865fb29f9f08edde95b51f5
inon56 <62841207+inon56@users.noreply.github.com>
2021-01-11 16:14:16 +0200

    Add files via upload

---
+++ maman_23/maman_23/example_.c
@@ -0,0 +1,25 @@
+
+
+this is first line
+second line
+
+#include "header1.h"
+
+after include header1   /*
+remove_comments: take source file and destination file and copy
+ all source code while omitting comments
+*/
+
+   #include "badinclude.h"   because
+#include notgood.h
+		after some comment
+                >> /*/ this is in comment
+still in comment
+**** commentttt ///////
+*/
+
+      #include          "header2.h"
+
+end of file
+
+/*last comment*/
